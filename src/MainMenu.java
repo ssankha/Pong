417c909c7830333dcf2f85cb58c83dd8ade16842
ssankha <ssankha@purdue.edu>
2020-12-16 02:59:41 -0800

    Displays the words "PLAY" and "QUIT"

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -7,11 +7,11 @@ public class MainMenu {
     private int titleX = Game.WIDTH/2 - (700/2);
     private int titleY = Game.HEIGHT / 4;
 
-    private int playX;
-    private int playY;
+    private int playX = Game.WIDTH / 2 - (219 / 2);
+    private int playY = Game.HEIGHT / 2;
 
-    private int quitX;
-    private int quitY;
+    private int quitX = playX;
+    private int quitY = playY + 100;
 
     public MainMenu(Game game) {
         this.game = game;
@@ -21,12 +21,14 @@ public class MainMenu {
         g.setColor(Color.white);
         g.setFont(new Font("Arial",Font.BOLD,150));
         Graphics2D g2 = (Graphics2D) g;
-
-        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
-                RenderingHints.VALUE_TEXT_ANTIALIAS_GASP);
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_GASP);
         g2.setStroke(new BasicStroke(10));
+
         g2.drawString("< PONG >", titleX, titleY);
 
-        System.out.println( g2.getFontMetrics().stringWidth("< PONG >"));
+        g.setFont(new Font("Arial",Font.BOLD,50));
+
+        g2.drawString("< PLAY >", playX, playY);
+        g2.drawString("< QUIT >", quitX, quitY);
     }
 }
