20dc5608c4be55919db5f2f0419ce5d8a3a7306f
ssankha <ssankha@purdue.edu>
2020-12-16 05:11:40 -0800

    Added functionality for "PLAY" and "QUIT"
    Text enlarges when hovered over using MouseMoved()

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.MouseEvent;
+
 
 public class MainMenu {
 
@@ -9,9 +11,14 @@ public class MainMenu {
 
     private int playX = Game.WIDTH / 2 - (219 / 2);
     private int playY = Game.HEIGHT / 2;
+    private int enlargedPlayX = Game.WIDTH/2 - (308/2);
 
     private int quitX = playX;
     private int quitY = playY + 100;
+    private int enlargedQuitX = enlargedPlayX;
+
+    private boolean playHover = false;
+    private boolean quitHover = false;
 
     public MainMenu(Game game) {
         this.game = game;
@@ -25,10 +32,58 @@ public class MainMenu {
         g2.setStroke(new BasicStroke(10));
 
         g2.drawString("< PONG >", titleX, titleY);
+        
 
-        g.setFont(new Font("Arial",Font.BOLD,50));
+        if(!playHover) {
+            g.setFont(new Font("Arial",Font.BOLD,50));
+            g2.drawString("< PLAY >", playX, playY);
+        }
+        else {
+            g.setFont(new Font("Arial",Font.BOLD,70));
+            g2.drawString("< PLAY >", enlargedPlayX, playY);
+        }
 
-        g2.drawString("< PLAY >", playX, playY);
-        g2.drawString("< QUIT >", quitX, quitY);
+        if(!quitHover) {
+            g.setFont(new Font("Arial",Font.BOLD,50));
+            g2.drawString("< QUIT >", quitX, quitY);
+        }
+        else {
+            g.setFont(new Font("Arial",Font.BOLD,70));
+            g2.drawString("< QUIT >", enlargedQuitX, quitY);
+        }
+
+    }
+
+    public void mouseMoved(MouseEvent e) {
+        Rectangle playRect = new Rectangle(playX, playY - 58, 219, 58);
+        Rectangle quitRect = new Rectangle(quitX, quitY - 58, 219, 58);
+
+        if(playRect.contains(e.getX(), e.getY())) {
+            playHover = true;
+        }
+        else {
+            playHover = false;
+        }
+
+        if(quitRect.contains(e.getX(), e.getY())) {
+            quitHover = true;
+        }
+        else {
+            quitHover = false;
+        }
+    }
+
+    public void mousePressed(MouseEvent e) {
+        Rectangle playRect = new Rectangle(playX, playY - 58, 219, 58);
+        Rectangle quitRect = new Rectangle(quitX, quitY - 58, 219, 58);
+
+        if(playRect.contains(e.getX(), e.getY())) {
+            Panel.status = "GAME";
+        }
+
+        if(quitRect.contains(e.getX(), e.getY())) {
+            System.exit(0);
+        }
+
     }
 }
