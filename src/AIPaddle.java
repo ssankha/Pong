e4075d069bcaf4270a3be5d07c3b73d3606ace5d
ssankha <ssankha@purdue.edu>
2020-12-15 21:38:55 -0800

    Changed shape from Rect to roundRect

--- src/AIPaddle.java
+++ src/AIPaddle.java
@@ -53,14 +53,7 @@ public class AIPaddle {
 
     public void paint(Graphics g) {
         g.setColor(Color.white);
-        g.fillRect(x, y, PADDLE_WIDTH, PADDLE_HEIGHT);
-
-//        try {
-//            BufferedImage image = ImageIO.read(new FileInputStream("police_officer.png"));
-//            g.drawImage(image, x, y, PADDLE_WIDTH, PADDLE_HEIGHT, null);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        g.fillRoundRect(x, y, PADDLE_WIDTH, PADDLE_HEIGHT, 10, 10);
     }
 
     public int getX() {
