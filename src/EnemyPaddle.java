03d331ce3a2f68ee1ca6dff04d29a48d9716f5d6
ssankha <ssankha@purdue.edu>
2020-12-17 23:08:22 -0800

    Added shoot() but causes ConcurrentaModification

--- src/EnemyPaddle.java
+++ src/EnemyPaddle.java
@@ -1,6 +1,7 @@
 import java.awt.*;
+import java.util.ArrayList;
 
-public class EnemyPaddle {
+public class EnemyPaddle implements Entity {
 
     public static final int PADDLE_WIDTH = (int) (Game.WIDTH / 20.0);
     public static final int PADDLE_HEIGHT = (int) (Game.WIDTH / 7.0);
@@ -12,13 +13,17 @@ public class EnemyPaddle {
     private int y;
     private int vX;
     private int vY;
+    private Handler handler;
+    private int shootTimer;
 
-    public EnemyPaddle(Game game, int x, int y, int vX, int vY) {
+    public EnemyPaddle(Game game, Handler handler, int x, int y, int vX, int vY) {
         this.game = game;
+        this.handler = handler;
         this.x = x;
         this.y = y;
         this.vX = vX;
         this.vY = vY;
+        shootTimer = 0;
     }
 
     public void update() {
@@ -44,11 +49,23 @@ public class EnemyPaddle {
                 vX *= -1;
             }
         }
+
     }
 
     public void paint(Graphics g) {
         g.setColor(Color.white);
         g.fillRoundRect(x, y, PADDLE_WIDTH, PADDLE_HEIGHT, 10, 10);
+
+        shootTimer++;
+
+        if(shootTimer >= 200) {
+            shoot();
+            shootTimer -= 200;
+        }
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, PADDLE_WIDTH, PADDLE_HEIGHT);
     }
 
     public int getX() {
@@ -69,6 +86,17 @@ public class EnemyPaddle {
 
     public void setVY(int vY) {
         this.vY = vY;
+    }
+
+    public void shoot() {
+        if(vX == 0) {
+            synchronized (new Object()) {
+                handler.add(new Bullet(game, handler, x, y + PADDLE_HEIGHT / 2, 8, 0, 2));
+            }
+        }
+        else {
+            handler.add(new Bullet(game, handler, x + PADDLE_WIDTH / 2, y, 0, 8, 2));
+        }
     }
 }
 
