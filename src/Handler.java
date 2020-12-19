3f3d1b64d8e2732f61884ccbb63b5bc95e969fc6
ssankha <ssankha@purdue.edu>
2020-12-18 20:01:35 -0800

    Fixed concurrent modification error

--- src/Handler.java
+++ src/Handler.java
@@ -6,34 +6,60 @@ public class Handler {
 
     private Game game;
     private LinkedList<Entity> entities;
+    private LinkedList<Entity> toAdd;
+    private LinkedList<Entity> toRemove;
 
     public Handler(Game game) {
         this.game = game;
         entities = new LinkedList<Entity>();
+        toAdd = new LinkedList<Entity>();
+        toRemove = new LinkedList<Entity>();
     }
 
     public LinkedList<Entity> getEntities() {
         return entities;
     }
 
-    public void add(Entity e) {
+
+    public void addToEntities(Entity e) {
         entities.add(e);
     }
 
+
     public void update() {
+        entities.addAll(toAdd);
+        entities.removeAll(toRemove);
+
         for (Entity e : entities) {
             e.update();
         }
+
     }
 
     public void paint(Graphics g) {
+        entities.addAll(toAdd);
+        entities.removeAll(toRemove);
+
         for (Entity e : entities) {
             e.paint(g);
         }
 
     }
 
-    public void remove(Entity e) {
-        entities.remove(e);
+
+    public void addToRemove(Entity e) {
+        toRemove.add(e);
+    }
+
+    public LinkedList<Entity> getToRemove() {
+        return toRemove;
+    }
+
+    public void addToAdd(Entity e) {
+        toAdd.add(e);
+    }
+
+    public LinkedList<Entity> getToAdd() {
+        return toAdd;
     }
 }
